39c6ec8ed33f85814fb98f7af5bbb6faf85e86b5
blissfox-blue <renardromain@hotmail.fr>
2025-04-29 01:19:49 +0900

    feat: Link dyn. version from generated `__about__.py` in pkg `__init__.py`

--- src/rr_test_workflow/__init__.py
+++ src/rr_test_workflow/__init__.py
@@ -1,6 +1,11 @@
 """Test GitHub workflows, actions and more."""
 
 
+from . import __about__
+
+__version__ = __about__.__version__
+__version_tuple__ = __about__.__version_tuple__
+
 def main() -> None:
 	"""Say 'Hi'."""
 	print("Hello from test-workflow!")
